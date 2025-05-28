ed123db9650b47cc8ba631bb3a874fff113021fb
Leonardo Giampaglia Gomes <129779553+Giampaglia@users.noreply.github.com>
2025-05-28 17:27:32 -0300

    commit curso

---
+++ aula6/index.js
@@ -0,0 +1,7 @@
+let nome = "joao";
+
+console.log(nome, "nasceu em 1984");
+console.log("em 2000", nome, "conheceu maria");
+console.log(nome,"casou-se com maria em 2012");
+console.log("maria teve um filho com", nome ,"em 2015");
+console.log("o filho de",nome,"se chama eduardo");
---
+++ aula7/index.js
@@ -0,0 +1,4 @@
+const primeiroNumero = 20;
+const segundoNumero = 25;
+const conta = primeiroNumero + segundoNumero
+console.log(conta);
