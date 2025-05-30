38fd0ac30f39f78bb6851a3f6df7ba13d1574762
Leonardo Giampaglia Gomes <129779553+Giampaglia@users.noreply.github.com>
2025-05-30 18:18:59 -0300

    curso

---
+++ aula12/script.js
@@ -0,0 +1,5 @@
+let num1 = parseInt(prompt("Digite um numero:"));
+let num2 = parseInt(prompt("Digite outro numero:"));
+let soma = num1 + num2;
+console.log(soma);
+window.alert('O resultado da sua soma foi: '+ soma);
